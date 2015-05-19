7f015158e9eab2e5f96f7b92dc9053ae7157e9b7
Kristian Berg <kriberg@tihlde.org>
2015-05-19 21:37:59 +0000

    Add filter for use in img src to display images

---
+++ app/js/filters/eve-image.js
@@ -0,0 +1,13 @@
+function EveImage32() {
+    "use strict";
+    return function (typeID) {
+        return "https://image.eveonline.com/Type/" + typeID + "_32.png";
+    }
+}
+
+function EveImage64() {
+    "use strict";
+    return function (typeID) {
+        return "https://image.eveonline.com/Type/" + typeID + "_64.png";
+    }
+}
