cd70257ae3203556f0ad32d3f6b293dc34890a46
Kristian Berg <kriberg@tihlde.org>
2015-05-25 07:17:36 +0000

    Fix pagination for notifications and added icons

--- app/js/controllers/dashboard.js
+++ app/js/controllers/dashboard.js
@@ -4,27 +4,40 @@
     function DashboardController($scope, $interval, Stationspinner) {
         $scope.characters = Stationspinner.CharacterSheet.query();
         $scope.capsuler = Stationspinner.Capsuler.get();
-        $scope.notifications = Stationspinner.Notifications.query();
+        $scope.notifications = [];
+        $scope.notificationsPagination = {
+            currentPage: 1,
+            itemsPerPage: 8,
+            total: 0
+        };
         $scope.missingTraining = Stationspinner.MissingTraining.query();
         $scope.newest = true;
         $scope.oldest = false;
-        $scope.currentPage = 1;
         $scope.noCharacters = false;
         $scope.characters.$promise.then(function (characters) {
             if(characters.length < 1) {
                 $scope.noCharacters = true;
             }
         });
-        $scope.notifications.$promise.then(function() {
-            sidebar();
-        });
+
+        $scope.getNotificationsPage = function(pageNumber) {
+            Stationspinner.Notifications.query({
+                page: pageNumber
+            }, function(data) {
+                $scope.notifications = data.results;
+                $scope.notificationsPagination.total = data.count;
+                $scope.notificationsPagination.currentPage = pageNumber;
+                sidebar();
+            });
+        };
+        $scope.getNotificationsPage($scope.notificationsPagination.currentPage);
+
         sidebar();
 
         $scope.refresh = function () {
             $scope.characters = Stationspinner.CharacterSheet.query();
-            $scope.notifications = Stationspinner.Notifications.query();
+            $scope.notifications = $scope.getNotificationsPage($scope.notificationsPagination.currentPage);
             $scope.missingTraining = Stationspinner.MissingTraining.query();
-            sidebar();
         };
         $interval(function(){$scope.refresh();}, 300000);
     }
