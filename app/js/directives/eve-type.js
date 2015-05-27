c4ae3c9b1d6954b1620a9377d63d736be8f65cac
Kristian Berg <kriberg@tihlde.org>
2015-05-27 20:55:14 +0000

    Fix warning for non-assigneable variable to eve-type

--- app/js/directives/eve-type.js
+++ app/js/directives/eve-type.js
@@ -6,13 +6,11 @@
             restrict: 'E',
             scope: {
                 type: '=type',
-                hover: '=?hover',
-                icon: '=?icon'
+                hover: '=hover',
+                icon: '=icon'
             },
             templateUrl: 'js/directives/eve-type.html',
             controller: function($scope, Stationspinner) {
-                $scope.icon = $scope.icon || true;
-                $scope.hover = $scope.hover || false;
                 if($scope.hover) {
                     Stationspinner.InvType.get({typeID: $scope.type.typeID}).$promise.then(function(invType) {
                         $scope.description = invType.description;
